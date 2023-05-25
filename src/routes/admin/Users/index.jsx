412e6cd83ccb1a109e1883ec02f38165b827bdfb
Yuri <yuripeixinho03@gmail.com>
2023-05-25 15:04:38 -0300

    DELETE user

--- src/routes/admin/Users/index.jsx
+++ src/routes/admin/Users/index.jsx
@@ -6,10 +6,14 @@ import "./styles.scss";
 import AdminButtonsFooter from "../../../components/admin/AdminButtonsFooter";
 import AdminTable from "../../../components/admin/AdminTable";
 import AdminHeader from "../../../components/admin/AdminHeader";
+import { Popconfirm } from "antd";
+import { Trash } from "iconsax-react";
 
 export default function AdminUsers() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  const userService = new UserService();
 
   const tableHead = [
     {
@@ -23,12 +27,28 @@ export default function AdminUsers() {
       key: "name",
     },
     { title: "Email", dataIndex: "email", key: "email" },
+    {
+      title: "Ação",
+      key: "action",
+      render: (record) => (
+        <>
+          <div className="table-actions">
+            <Popconfirm
+              title="Remover usuário"
+              description={`Deseja o usuário "${record.name}"?`}
+              onConfirm={() => handleDelete(record)}
+            >
+              <Trash size="24" cursor="pointer" color="#f47373" />
+            </Popconfirm>
+          </div>
+        </>
+      ),
+    },
   ];
 
   useEffect(() => {
     async function init() {
       setLoading(true);
-      const userService = new UserService();
       const userResponse = await userService.list();
 
       setUsers(userResponse);
@@ -37,6 +57,18 @@ export default function AdminUsers() {
 
     init();
   }, []);
+
+  async function handleDelete(values) {
+    await userService
+      .delete(values.id)
+      .then(() => {
+        const updatedUsers = users.filter((round) => round.id !== values.id);
+        setUsers(updatedUsers);
+      })
+      .catch((err) => {
+        alert(err.msg);
+      });
+  }
 
   return (
     <div>
