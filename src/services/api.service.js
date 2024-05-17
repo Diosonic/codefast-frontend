e81bb7a92d794fd057e628625a60fa3023ef36f6
Yago Peixinho <peixinhoyago@gmail.com>
2024-05-17 17:20:42 -0300

    feat: Ajuste URL produção e tempo da eliminatória

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const url = "https://codefast-api-uninassau.azurewebsites.net/";
-const url = "http://localhost:5165/";
+const url = "https://codefast-api-uninassau.azurewebsites.net/";
 
 const api = axios.create({
   baseURL: url,
